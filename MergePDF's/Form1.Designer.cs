b582f9fff97b7fc13d1294e33a53e56dc730e1f7
williamanjo <william.anjodaguarda20@gmail.com>
2021-06-23 10:02:24 -0300

    Initial Commit :tada:

---
+++ MergePDF's/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace MergePDF_s
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Selecionar Pasta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(81, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PDF\'s em: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(81, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Selecionar Pasta";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Salvar em: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 399);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(525, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(525, 274);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(3, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 255);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(525, 20);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Iniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.HideSelection = false;
+            this.textBox3.Location = new System.Drawing.Point(3, 3);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(510, 249);
+            this.textBox3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 438);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MergePDF\'s";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ MergePDF's/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+
+namespace MergePDF_s
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WriteLog(textBox3, " - Selecione a Pasta Com os PDF's.");
+            WriteLog(textBox3, 
+                " - Pdf's no formato ( Arquivo-1.pdf, Arquivo-2.pdf, Arquivo-3.pdf )." + 
+                Environment.NewLine +
+                " ---------------------------------------------------------------------------------------------------------" + 
+                Environment.NewLine);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result.ToString() == "Ok")
+            {
+                
+                textBox1.Text = dialog.FileName;
+                WriteLog(textBox3, " - PDF's em : " + dialog.FileName);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if(result.ToString() == "Ok")
+            {
+                textBox2.Text = dialog.FileName;
+                WriteLog(textBox3, " - Salvar em : " + dialog.FileName);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                button3.Enabled = false;
+                WriteLog(textBox3,
+                    " ---------------------------------------------------------------------------------------------------------" +
+                    Environment.NewLine +
+                    " - Gerando Arquivos :" + Environment.NewLine +
+                    Environment.NewLine);
+                try
+                {
+
+
+                    DirectoryInfo directoryInfo = new DirectoryInfo(textBox1.Text);
+
+                    //Get the PDF files in folder path into FileInfo
+                    FileInfo[] files = directoryInfo.GetFiles("*.pdf");
+
+                    List<String> Files = new List<string>();
+                    foreach (FileInfo file in files)
+                    {
+                        if (file.Name.EndsWith("-1.pdf")) { Files.Add(file.Name.Replace("-1.pdf", "")); }
+                    }
+                    progressBar1.Maximum = Files.Count;
+                    progressBar1.Value = 0;
+                    foreach (String FileName in Files)
+                    {
+
+                        MergeFileAsync(FileName, directoryInfo);
+                        progressBar1.Value++;
+                    }
+                    button3.Enabled = true;
+                    WriteLog(textBox3, Environment.NewLine + " --- Finalizado ---");
+                }
+                catch (Exception error)
+                {
+                    WriteLog(textBox3, Environment.NewLine + " ( " + error + " ) ");
+                    button3.Enabled = true;
+                }
+            }
+            else
+            {
+                if (textBox1.Text == "")
+                {
+                    WriteLog(textBox3, "  * Selecione a Local com os PDF's * ");
+                }
+                if (textBox2.Text == "")
+                {
+                    WriteLog(textBox3, "  * Selecione o Local Para Salvar * ");
+                }
+            }
+        }
+        async void MergeFileAsync(String FileName, DirectoryInfo directoryInfo)
+        {
+            await MergeFile(FileName, directoryInfo);
+        }
+        private Task MergeFile(String FileName, DirectoryInfo directoryInfo)
+        {
+            //Create a new PDF document 
+            PdfDocument document = new PdfDocument();
+            document.EnableMemoryOptimization = true;
+
+            FileInfo[] files = directoryInfo.GetFiles(FileName + "*.pdf");
+            foreach (FileInfo file in files)
+            {
+                //Load the PDF document 
+                FileStream fileStream = new FileStream(file.FullName, FileMode.Open);
+                PdfLoadedDocument loadedDocument = new PdfLoadedDocument(fileStream);
+
+                //Merge PDF file
+                PdfDocumentBase.Merge(document, loadedDocument);
+
+                //Close the existing PDF document 
+                loadedDocument.Close(true);
+            }
+            //Save the PDF document
+            string filename = textBox2.Text + "\\" + FileName + ".pdf";
+
+            WriteLog(textBox3, "  § Created :" + filename);
+            FileStream doc = new FileStream(filename, FileMode.Create);
+            document.Save(doc);
+            document.Close(true);
+            Thread.Sleep(100);
+            return Task.CompletedTask;
+        }
+
+        public void WriteLog(TextBox tb, string log)
+        {
+            tb.AppendText(log + Environment.NewLine);
+        }
+    }
+}
